c711174ce5b1fe585af92c3807cd9626bcc7f8e6
Josh Faust <jfaustwg@willowgarage.com>
2009-08-28 02:25:10 +0000

    Convert generic_laser_filter_node to NodeHandle and MessageFilter
    git-svn-id: https://code.ros.org/svn/ros-pkg/pkg/trunk/stacks/laser_pipeline@23220 eb33c2ac-9c88-4c90-87e0-44a10359b0c3

--- laser_scan/src/generic_laser_filter_node.cpp
+++ laser_scan/src/generic_laser_filter_node.cpp
@@ -27,57 +27,61 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "ros/node.h"
+#include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
 #include "laser_scan/median_filter.h"
 #include "laser_scan/intensity_filter.h"
 #include "laser_scan/scan_shadows_filter.h"
 #include "laser_scan/footprint_filter.h"
-#include "tf/message_notifier.h"
+#include <message_filters/subscriber.h>
+#include "tf/message_filter.h"
 
 
 class GenericLaserScanFilterNode 
 {
 public:
-  GenericLaserScanFilterNode(ros::Node& anode) :  filter_chain_("sensor_msgs::LaserScan"), node_(anode), notifier_(NULL)
+  GenericLaserScanFilterNode() 
+    :  filter_chain_("sensor_msgs::LaserScan")
+    , scan_sub_(nh_, "scan_in", 50)
+						  
   {
-    node_.advertise<sensor_msgs::LaserScan>("~output", 1000);
-    notifier_ = new tf::MessageNotifier<sensor_msgs::LaserScan>(&tf_, &node_, 
-        boost::bind(&GenericLaserScanFilterNode::callback, this, _1), "scan_in", "base_link", 50);
-    notifier_->setTolerance(ros::Duration(0.03));
+    ros::NodeHandle local_nh("~");
+    output_pub_ = local_nh.advertise<sensor_msgs::LaserScan>("output", 1000);
+    tf_filter_ = new tf::MessageFilter<sensor_msgs::LaserScan>(scan_sub_, tf_, "base_link", 50);
+    tf_filter_->registerCallback(boost::bind(&GenericLaserScanFilterNode::callback, this, _1));
+    tf_filter_->setTolerance(ros::Duration(0.03));
 
     filter_chain_.configure("~");
-    //node_.subscribe("scan_in", msg, &GenericLaserScanFilterNode::callback,this, 3);
   }
 
   ~GenericLaserScanFilterNode(){
-    if(notifier_)
-      delete notifier_;
+    delete tf_filter_;
   }
 
-  void callback(const tf::MessageNotifier<sensor_msgs::LaserScan>::MessagePtr& msg_in)
+  void callback(const sensor_msgs::LaserScanConstPtr& msg_in)
   {
-    filter_chain_.update (*msg_in, msg);
-    node_.publish("~output", msg);
+    filter_chain_.update (*msg_in, msg_);
+    output_pub_.publish(msg_);
   }
 
 protected:
   filters::FilterChain<sensor_msgs::LaserScan> filter_chain_;
-  ros::Node& node_;
+  ros::NodeHandle nh_;
   tf::TransformListener tf_;
-  tf::MessageNotifier<sensor_msgs::LaserScan>* notifier_;
-  sensor_msgs::LaserScan msg;
+  message_filters::Subscriber<sensor_msgs::LaserScan> scan_sub_;
+  tf::MessageFilter<sensor_msgs::LaserScan>* tf_filter_;
+  ros::Publisher output_pub_;
+  sensor_msgs::LaserScan msg_;
 
 
 };
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv);
-  ros::Node ros_node("scan_filter_node");
+  ros::init(argc, argv, "scan_filter_node");
   
-  GenericLaserScanFilterNode t(ros_node);
-  ros_node.spin();
+  GenericLaserScanFilterNode t;
+  ros::spin();
   
   return 0;
 }
